a3efa51704084e1b295067a8414115c99db7cdb9
Abdi Tefera <abditefera11@gmail.com>
2024-11-29 13:21:30 +0300

    Dashboard section is completed includes sidebar, header, and layout that can wrap other componets

---
+++ frontend/config/menuItems.jsx
@@ -0,0 +1,81 @@
+import { 
+    FiGrid, 
+    FiDollarSign,
+    FiClock,
+    FiPackage,
+    FiSettings,
+  } from 'react-icons/fi';
+  import {FaSchool, FaRegPlusSquare} from 'react-icons/fa';
+
+
+export const MENU_ITEMS = [
+  {
+    id: 'dashboard',
+    icon: FiGrid,
+    label: 'Dashboard',
+    href: '/dashboard',
+    roles: ""
+  },
+  {
+    id: 'school',
+    icon: FaSchool,
+    label: 'School',
+    href: '/school',
+    roles: "",
+  },
+  {
+    id: 'createschool',
+    icon: FaRegPlusSquare,
+    label: 'Create School',
+    href: '/createschool',
+    roles: "",
+  },
+  {
+    id: 'subscription',
+    icon: FiDollarSign,
+    label: 'Subscription',
+    href: '/subscription',
+    roles: "",
+  },
+  {
+    id: 'pandingrequest',
+    icon: FiClock,
+    label: 'Pending Request',
+    href: '/pandingrequest',
+    roles: "",
+  },
+  {
+    id: 'package',
+    icon: FiPackage,
+    label: 'Package',
+    href: '/package',
+    roles: "",
+  },
+  {
+    id: 'setting',
+    icon: FiSettings,
+    label: 'Settings',
+    href: '/setting',
+    roles: "",
+    subItems: [
+      {
+        id: 'system',
+        label: 'System Setting',
+        href: '/setting/system',
+        roles: ""
+      },
+      {
+        id: 'payment ',
+        label: 'Payment Setting',
+        href: '/setting/payment',
+        roles: ""
+      },
+      {
+        id: 'about',
+        label: 'About',
+        href: '/setting/about',
+        roles: ""
+      }
+    ]
+  },
+];
