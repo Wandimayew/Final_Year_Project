5813f8c80901aa68ce27f7e62cf3f0e6a4cda5e1
Abdi Tefera <abditefera11@gmail.com>
2024-11-29 19:39:58 +0300

    createschool form component is completed and sidebar, header components are updated

---
+++ frontend/app/school/createschool/page.jsx
@@ -0,0 +1,13 @@
+import CreateSchool from '@/components/school/CreateSchool'
+import Layout from '@/components/sidebar/Layout'
+import React from 'react'
+
+const Create = () => {
+  return (
+    <Layout>
+        <CreateSchool/>
+    </Layout>
+  )
+}
+
+export default Create
